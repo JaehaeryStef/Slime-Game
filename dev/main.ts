ed2b350c255d1149e2de85b52eb8ba9c8d8f09eb
Damien <abbemad@gmail.com>
2017-06-13 00:40:20 +0200

    Start game button + Logo

--- dev/main.ts
+++ dev/main.ts
@@ -1,3 +1,22 @@
+/// <reference path="game.ts"/>
+
+var title: HTMLElement;
+var start: HTMLElement;
+
 window.addEventListener("load", function() {
-    new Game();
+
+    title = document.createElement("gamename");
+    document.body.appendChild(title);
+
+    start = document.createElement("start");
+     start.addEventListener("click", function () {
+
+        title.remove();
+        start.remove();
+
+        new Game();
+    });
+    
+    document.body.appendChild(start);
 });
+
--- docs/js/main.js
+++ docs/js/main.js
@@ -136,8 +136,18 @@ var Gameobjects = (function () {
     }
     return Gameobjects;
 }());
+var title;
+var start;
 window.addEventListener("load", function () {
-    new Game();
+    title = document.createElement("gamename");
+    document.body.appendChild(title);
+    start = document.createElement("start");
+    start.addEventListener("click", function () {
+        title.remove();
+        start.remove();
+        new Game();
+    });
+    document.body.appendChild(start);
 });
 var Snotspawn = (function (_super) {
     __extends(Snotspawn, _super);
